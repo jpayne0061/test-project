d8d3ccfdeb6aebf430550b0d62eead59e8be5ebf
Evan Payne <jpayne0061@gmail.com>
2014-03-01 20:20:46 -0500

    random shuffle

--- sorter.rb
+++ sorter.rb
@@ -1,39 +1,15 @@
 
-def a d
-  evan d, []
+def shuffle a
+  b = []
+  z = a.length
+	while z > 0	  
+	  x = a[rand(a.length)]
+		b.push x
+		a.delete(x)
+		z = z -1
+  end
+	print b
 end
-
-def sorter a, b
-  if a.length <= 0
-    return b
-	
-  end	
-  
-    smallest = a.pop
-    c = []
-  
-	a.each do |x|
-      if x < smallest
-	    c.push smallest
-		smallest = x
-	  else
-        c.push x
-      end		
-    end 
-  
-  b.push smallest
-  evan c, b
-  
-end
-
-puts (a(['2', '3', '1', '7', '4']))
-
-
-
-
-
-
-
 
 
 
