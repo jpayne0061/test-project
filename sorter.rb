69384f4244ec428ba6215f8af76a4f8e9b3b54b3
jpayne0061 <jpayne0061@gmail.com>
2014-03-05 11:41:07 -0500

    Update sorter.rb

--- sorter.rb
+++ sorter.rb
@@ -1,14 +1,13 @@
-
 def shuffle a
   b = []
   z = a.length
-	while z > 0	  
-	  x = a[rand(a.length)]
-		b.push x
-		a.delete(x)
-		z = z -1
-  end
-	print b
+    while z > 0	  
+      x = a[rand(a.length)]
+	b.push x
+	a.delete(x)
+	z = z -1
+    end
+  b
 end
 
 
