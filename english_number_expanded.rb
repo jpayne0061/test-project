17c9890f482a87c57b3514f313f0b2b960f5f384
Evan Payne <jpayne0061@gmail.com>
2014-03-06 13:23:50 -0500

    english number expanded to trillions

---
+++ english_number_expanded.rb
@@ -0,0 +1,104 @@
+def english_number(number)
+	if number < 0
+		return 'please enter a number greater than 0'
+	end
+  if number == 0
+    return 'zero'  
+  end  
+  
+  num_string = ''
+  
+  ones  = ['one','two','three','four','five','six','seven','eight','nine']
+  tens  = ['ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety']
+  teens = ['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+  
+  left  = number
+  write = left/1000000000000
+  left  = left - write*1000000000000
+  
+  if write > 0
+    trillions = english_number(write)
+    num_string = num_string + trillions + ' trillion,'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+  
+  write = left/1000000000
+  left  = left - write*1000000000
+  
+  if write > 0
+    billions = english_number(write)
+    num_string = num_string + billions + ' billion,'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end  
+  
+  write = left/1000000
+  left  = left - write*1000000
+  
+  if write > 0
+    millions = english_number(write)
+    num_string = num_string + millions + ' million,'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+  
+  
+  write = left/1000
+  left  = left - write*1000
+  
+  if write > 0
+    thousands = english_number(write)
+    num_string = num_string + thousands + ' thousand,'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+  
+  write = left/100
+  left  = left - write*100
+  
+  if write > 0
+    hundreds = english_number(write)
+    num_string = num_string + hundreds + ' hundred'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
+  
+  write = left/10
+  left  = left - write*10
+  
+  if write > 0
+    if ((write == 1) and (left > 0))
+      num_string = num_string + teens[left-1]
+      left = 0
+    else
+      num_string = num_string + tens[write-1]  
+    end  
+    
+    if left > 0
+      num_string = num_string + '-'
+    end
+  end
+    
+    write = left
+    left = 0
+    
+  if write > 0  
+    num_string = num_string + ones[write-1]
+  end
+  
+  num_string
+end
+
+
+puts english_number(23468733687)
+
+
+
+
+
